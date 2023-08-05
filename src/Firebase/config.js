7dcd9bc41cf7d31b0c31a32b395f8b4bf3cb8e36
Anthony Ahrens <anthony.l.ahrens@gmail.com>
2023-08-05 17:01:28 -0500

    Removed initlial boiler plate code. Setup Firebase configuration file.

---
+++ src/Firebase/config.js
@@ -0,0 +1,21 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyA-kRLhrk1V443tSVXk3polWgRfGmLyjSc",
+    authDomain: "fir-vue-f1bdc.firebaseapp.com",
+    projectId: "fir-vue-f1bdc",
+    storageBucket: "fir-vue-f1bdc.appspot.com",
+    messagingSenderId: "254218786747",
+    appId: "1:254218786747:web:6556be2ac99be631d3169e",
+    measurementId: "G-B6FH92TDY7"
+  };
+
+//init firebase
+firebase.initializeApp(firebaseConfig)
+
+const projectFirestore = firebase.firestore()
+const timestamp = firebase.firestore.FieldValue.serverTimestamp
+
+export { projectFirestore, timestamp }
